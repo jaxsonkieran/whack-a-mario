8d6c3711a398f31cfc4e74a8eaa84930e133beac
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-17 13:44:13 +0000

    add hammer as cursor to html, style and add image to assets folder

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// create function that counts down time
+//  create function that holds the score
+// create function that produces the mario in a random hole
+
+const cursor = document.querySelector('#mario-hammer');
+
