47262fdcd59567d04aeb80b2a5ee9c40a5974805
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-17 14:26:03 +0000

    add hammer to cursor on mousemove

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,18 @@
 //     cursor.style.top = e.pageY
 //     cursor.style.left = e.pageX
 // });
+
+
+// Code with help of freecodecamp - https://www.freecodecamp.org/news/how-to-make-a-custom-mouse-cursor-with-css-and-javascript/
+const cursorHammer = document.querySelector('.hammer');
+
+
+const moveCursor = (e)=> {
+  const mouseY = e.clientY;
+  const mouseX = e.clientX;
+   
+  cursorHammer.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
+   
+}
+
+window.addEventListener('mousemove', moveCursor);
