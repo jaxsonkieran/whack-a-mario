39ed93e274548550709214e963ce2b144e0c47db
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-17 18:08:03 +0000

    add another function comment for the mario up css class

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 // create function that counts down time
 //  create function that holds the score
 // create function that produces the mario in a random hole
+// create a function to get the mario to peep up css classList add 'up'
+
 
