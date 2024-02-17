11193f0dc2c33a3b3cc52a6c39959e15d3bad60c
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-17 18:06:01 +0000

    remove comments for cursor change in js file

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,25 +2,3 @@
 //  create function that holds the score
 // create function that produces the mario in a random hole
 
-// const cursor = document.querySelector('.hammer');
-
-// window.addEventListener('mousemove', e => {
-//     cursor.style.top = e.pageY
-//     cursor.style.left = e.pageX
-// });
-
-
-// Code with help of freecodecamp - https://www.freecodecamp.org/news/how-to-make-a-custom-mouse-cursor-with-css-and-javascript/
-// const cursorHammer = document.querySelector('.hammer');
-
-// window.addEventListener('mousemove', e => {
-//   cursorHammer.style.top = e.pageY + 'px'
-//   cursorHammer.style.left = e.pageX + 'px'
-// })
-// const moveCursor = (e)=> {
-//   const mouseY = e.clientY;
-//   const mouseX = e.clientX;
-   
-//   cursorHammer.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-   
-// }
