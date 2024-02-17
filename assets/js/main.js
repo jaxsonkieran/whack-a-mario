28ca06bf9681051e268562f26b6f3e8d51845f3c
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-17 18:17:29 +0000

    testing js function for random hole

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,4 +3,10 @@
 // create function that produces the mario in a random hole
 // create a function to get the mario to peep up css classList add 'up'
 
+// Declare game variables
+const hole = document.querySelectorAll('.hole');
 
+console.log(hole.length);
+/**
+ * function to get random holes/pipes for the mario to appear in
+ */
