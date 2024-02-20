882446ea4c13bb2cd0059af99396def3fdec310a
Kieran Jackson <jaxsonkieran@gmail.com>
2024-02-20 16:35:56 +0000

    fix: add a point to the score on every 'mousedown' hit on the mario

--- assets/js/main.js
+++ assets/js/main.js
@@ -12,7 +12,6 @@ const mario = document.querySelectorAll('.mario');
 
 let result = 0;
 let hitTheMario;
-let currentTime = 60;
 
 
 /**
@@ -27,11 +26,29 @@ function randomHole () {
 // get a random number from 0 - 8, (1-9), start counting from 0
     let randomHole = holes[Math.floor(Math.random() * 9)];
     randomHole.classList.add('mario');
+
+    hitTheMario = randomHole.id
 }
+
+// Now I need a function that will record the hit or 'mousedown' on the mario each time it appears
+/**
+ * function that will record a hit on mousedown and add a point for every hit to the scoreboard
+ */
+holes.forEach(hole => {
+    hole.addEventListener('mousedown', () => {
+        if (hole.id === hitTheMario){
+            result++
+            score.textContent = result
+            hitTheMario = null
+        }
+    })
+})
 
 /**
  * Function to get the mario to move at a set interval
  */
+
+// I will assign this to the play button
 
 function moveMario () {
     let timerId = null
@@ -40,12 +57,8 @@ function moveMario () {
 
 moveMario();
 
-// randomHole();
 
 
-// Now I need a function that will record the hit or 'mousedown' on the mario each time it appears
-/**
- * function that will record a hit on mousedown and add a point for every hit to the scoreboard
- */
+
 
 
